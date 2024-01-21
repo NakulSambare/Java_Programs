7ecdbbf981fca8997a7040d1e11a0e6a83f173b9
Nakul Sambare <sambarenakul@gmail.com>
2024-01-21 11:01:45 +0530

    Create Main.java
    initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hellow world");
+        System.out.println("Todays date is : "+new Date());
+        System.out.println(" date is "+new Date().getTime());
+    }
+}
